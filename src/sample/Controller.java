50159b358dd50157a109bb61b7eedf6941b49dbd
dori <dori.ljuta@hotmail.com>
2019-02-02 23:07:18 +0100

    added JDBC

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,6 +2,9 @@ package sample;
 
 import javafx.scene.control.TextField;
 import com.google.gson.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -19,20 +22,26 @@ public class Controller {
     public void getData() throws Exception {
         //2. Convert object to JSON string and save into a file directly
         String JsonString = getHTML("http://transport.opendata.ch/v1/connections?from=Winterthur&to=Stettbach").replace(" ", "");
-        JsonParser parser = new JsonParser();
-        JsonElement traindata = parser.parse(JsonString);
-        JsonArray responseData = traindata.getAsJsonArray();
+        //JsonParser parser = new JsonParser();
+        //JsonElement traindata = parser.parse(JsonString);
+        //JsonArray responseData = traindata.getAsJsonArray();
+        JSONObject myjson;
+        try
+        {
+            myjson = new JSONObject(JsonString);
+            JSONObject jsonObj1 = myjson.getJSONObject("station");
+            System.out.println(jsonObj1.getJSONObject("name"));
+            System.out.println("here ===>>>"+jsonObj1.getJSONObject("name").get("$t").toString());
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
 
         //JsonArray arr = new JsonArray(traindata);
         //List<String> list = new ArrayList<String>();
         //for(int i = 0; i < arr.size(); i++){
         //    list.add(arr.getJsonObject(i).getString("from"));
         //}
-
-        JsonObject connections = new Gson().fromJson(responseData.get(0), JsonObject.class);
-        JsonObject from = new Gson().fromJson(connections.get("arrival"), JsonObject.class);
-        from.toString();
-        System.out.println(from);
 
 
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.*;
+import java.util.ArrayList;
+
 public class Main extends Application {
 
     @Override
@@ -15,6 +18,39 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
+
+    private ArrayList<String> connections = new ArrayList<>();
+
+    try {
+        // Driver class für MySql-Datenbank laden
+        Class.forName("com.mysql.jdbc.Driver");
+
+        // Verbindung zur Datenbank herstellen
+        String connectionUrl = "jdbc:mysql://localhost/fahrplanDB?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+        Connection connection = DriverManager.getConnection(connectionUrl, "root", "");
+
+        // Sql Befehl aufbauen und ausführen
+        Statement stmt = connection.createStatement();
+        ResultSet entries = stmt.executeQuery("SELECT * FROM fahrplan");
+
+        // alle Datensätze aus dem ResultSet auslesen und
+        // (Schritt 1) in einer ArrayList ablegen
+        while (entries.next()) {
+            connections.add(entries.getString("text"));
+        }
+
+        // alle verwendeten Objekte schliessen
+        entries.close();
+        stmt.close();
+        connection.close();
+    } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+    } catch (
+    SQLException e) {
+        e.printStackTrace();
+    }
+
+}
 
 
     public static void main(String[] args) {
